92ea0e1a51af1fa1c8145efeb2db3f431db1e721
Artur <a3332133@gmail.com>
2018-04-05 14:56:59 +0300

    project rename

---
+++ SubRosa.dpr
@@ -0,0 +1,21 @@
+program SubRosa;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  Unit2 in 'Unit2.pas' {Form2},
+  Vcl.Themes,
+  Vcl.Styles,
+  Unit3 in 'Unit3.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Amethyst Kamri');
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
