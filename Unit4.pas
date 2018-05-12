b99a2b2e65a589f4e7cbaf2e46153e06dd10d22c
rutraq <a3332133@gmail.com>
2018-05-12 11:44:01 +0300

    USB changes

---
+++ Unit4.pas
@@ -0,0 +1,133 @@
+unit Unit4;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm4 = class(TForm)
+    Button1: TButton;
+    ComboBox1: TComboBox;
+    Memo1: TMemo;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+  k:integer;
+
+implementation
+
+{$R *.dfm}
+
+uses Unit1;
+function FileSetHidden(const FileName: string; hid: Boolean): Boolean;
+var Flags: Integer;
+begin
+  Result := False;
+  Flags := GetFileAttributes(PChar(FileName));
+  if hid then
+  Flags := Flags or faHidden
+  else
+  Flags := Flags and not faHidden;
+
+  Result := SetFileAttributes(PChar(FileName), Flags);
+end;
+
+procedure TForm4.Button1Click(Sender: TObject);
+var
+  Drive: Char; //Буква диска
+const
+  pref = ':\';
+begin
+  Combobox1.Items.Clear;
+    for Drive := 'B' to 'Z' do
+      begin
+        if GetDriveType(PChar(Drive + pref)) = DRIVE_REMOVABLE then Combobox1.Items.Add(Drive + pref);
+      end;
+  ComboBox1.Visible := True;
+  k := 0;
+end;
+
+procedure TForm4.Button2Click(Sender: TObject);
+var
+  Drive: Char; //Буква диска
+const
+  pref = ':\';
+begin
+  Combobox1.Items.Clear;
+    for Drive := 'B' to 'Z' do
+      begin
+        if GetDriveType(PChar(Drive + pref)) = DRIVE_REMOVABLE then Combobox1.Items.Add(Drive + pref);
+      end;
+  ComboBox1.Visible := True;
+  k := 1;
+end;
+
+procedure TForm4.ComboBox1Change(Sender: TObject);
+var r:integer;
+key1, key2:string;
+begin
+  if k = 0 then
+    begin
+      Memo1.Clear;
+      r := random(1000000);
+      Memo1.Lines.Add(IntToStr(r));
+      Memo1.Lines.SaveToFile(ComboBox1.Text + 'key.txt');
+      CreateDir('C:\Log Files');
+      Memo1.Lines.SaveToFile('C:\Log Files\KeyFile.txt');
+      FileSetHidden('C:\Log Files\KeyFile.txt', true);
+      FileSetHidden(ComboBox1.Text + 'key.txt', true);
+      ShowMessage('Ключевой USB-носитель создан');
+      Form4.Hide;
+      Form1.Show;
+    end
+      else
+        begin
+          Memo1.Clear;
+          Memo1.Lines.LoadFromFile('C:\Log Files\KeyFile.txt');
+          key1 := Memo1.Lines.Text;
+          Memo1.Clear;
+            if FileExists(ComboBox1.Text + 'key.txt') then
+              begin
+                Memo1.Lines.LoadFromFile(ComboBox1.Text + 'key.txt');
+                key2 := Memo1.Lines.Text;
+              end
+                else
+                  begin
+                    MessageDlg('Не является ключевым USB', mtWarning, [mbOk], 0);
+                    Form4.Close;
+                  end;
+          if key1 = key2 then
+            begin
+              Form4.Hide;
+              Form1.Show;
+            end
+              else
+                begin
+                  MessageDlg('Не является ключевым USB', mtWarning, [mbOk], 0);
+                  Form4.Close;
+                end;
+        end;
+end;
+
+procedure TForm4.FormCreate(Sender: TObject);
+begin
+  if FileExists('C:\Log Files\KeyFile.txt') then
+    begin
+      Button1.Visible := False;
+    end
+      else Button2.Visible := False;
+end;
+
+end.
---
+++ __recovery/Unit1.pas
@@ -0,0 +1,49 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses Unit2, Unit3;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Form1.Hide;
+  Form2.Show;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Form1.Hide;
+  Form3.Show;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  Application.Terminate;
+end;
+
+end.
