45207252ad8bd503a3bc9101b608d2b99333e888
rutraq <a3332133@gmail.com>
2018-06-07 12:33:30 +0300

    .

--- Unit2.pas
+++ Unit2.pas
@@ -2,6 +2,7 @@
 
 interface
 
+
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.Menus,
